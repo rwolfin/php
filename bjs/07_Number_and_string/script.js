06ec8293b4d5c726f2b1731d227c6687ee299111
Роман <info@web-vl.ru>
2024-12-30 19:23:29 +0300

    edit project

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -4,6 +4,33 @@ let operation = null;
 const inputWindow = document.getElementById('inputWindow');
 
 
+
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+
+document.getElementById('btn_sum').addEventListener('click', function() {
+    lastOperand = parseInt(inputWindow.value);
+    operation ='sum';
+    inputWindow.value = '';
+    console.log(lastOperand);
+})
+
+
+document.getElementById('btn_calc').addEventListener('click', function(){
+    if (operation === 'sum'){
+        const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+
+
+
+})
+
+
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
