327fdde68c66daafeb866d050a4237a3304cdc3e
Роман <info@web-vl.ru>
2025-01-13 22:34:28 +0300

    update

--- bjs/10_function_object/js/init.js
+++ bjs/10_function_object/js/init.js
@@ -16,9 +16,19 @@ window.onload = function() {
      function updatePerson() {
             const initPerson = personGenerator.getPerson();
             document.getElementById('surnameOutput').innerText = initPerson.surname;
-            document.getElementById('firstNameOutput').innerText = initPerson.firstName + " " + initPerson.middleName;
-            document.getElementById('genderOutput').innerText = initPerson.gender;
-            document.getElementById('birthYearOutput').innerText = `${initPerson.birthDay} ${initPerson.birthMonth} ${initPerson.birthYear} года, профессия ${initPerson.profession}`;
+             document.getElementById('firstNameOutput').innerText = initPerson.firstName + " " + initPerson.middleName;
+             document.getElementById('genderOutput').innerText = initPerson.gender;
+             document.getElementById('birthYearOutput').innerText = `${initPerson.birthDay} ${initPerson.birthMonth} ${initPerson.birthYear} года, профессия ${initPerson.profession}`;
+
+            //Обновляем картинку
+             const genderImage = document.getElementById('genderImage');
+            if (initPerson.gender === personGenerator.GENDER_MALE) {
+                genderImage.src = 'img/man.png';
+            } else {
+                genderImage.src = 'img/woman.png';
+            }
+
+
         }
     updatePerson();
 
@@ -26,10 +36,11 @@ window.onload = function() {
 
 
    clearButton.addEventListener('click', function(){
-           document.getElementById('surnameOutput').innerText = "Генерация фамилии";
+           document.getElementById('surnameOutput').innerText = "Генератор пользователей";
            document.getElementById('firstNameOutput').innerText = "Имя";
           document.getElementById('genderOutput').innerText = "Генерация пола";
            document.getElementById('birthYearOutput').innerText = "Генерация года рождения";
+            document.getElementById('genderImage').src = 'img/floor.png'
    });
 
 
